af2fb64d582ef8a98a04ead8b1519039e25a3fda
Everett Blakley <eblakley@appcolony.ca>
2021-05-02 01:35:36 -0600

    Working on the menu. Looking pretty good, not gonna lie

---
+++ src/components/Close.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    clip-rule="evenodd"
+    fill-rule="evenodd"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-miterlimit="5"
+    viewBox="0 0 32 32"
+  >
+    <path fill="none" d="M0 0h32v32H0z" />
+    <path
+      d="M5.49 4.49a1 1 0 00-.697 1.717L14.586 16l-9.793 9.793a1 1 0 101.414 1.414L16 17.414l9.793 9.793a1 1 0 101.414-1.414L17.414 16l9.793-9.793a1 1 0 00-.726-1.717 1 1 0 00-.688.303L16 14.586 6.207 4.793a1 1 0 00-.717-.303z"
+      clip-rule="nonzero"
+      color="#000"
+      fill-rule="nonzero"
+      style="
+        font-feature-settings: normal;
+        font-variant-alternates: normal;
+        font-variant-caps: normal;
+        font-variant-ligatures: normal;
+        font-variant-numeric: normal;
+        font-variant-position: normal;
+        isolation: auto;
+        mix-blend-mode: normal;
+        shape-padding: 0;
+        text-decoration-color: #000;
+        text-decoration-line: none;
+        text-decoration-style: solid;
+        text-indent: 0;
+        text-orientation: mixed;
+        text-transform: none;
+        white-space: normal;
+      "
+    />
+  </svg>
+</template>
+<script>
+export default {
+  name: "Close",
+};
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,26 +2,69 @@
   <div
     class="w-full mx-auto h-10 flex items-center px-8 py-8 bg-transparent absolute top-0 left-1/2 transform -translate-x-1/2 z-50"
   >
-    <div>
+    <div class="relative z-20">
       <router-link to="/"><h5>FedEmAt</h5></router-link>
     </div>
     <div class="ml-auto relative">
       <div class="md:hidden">
-        <hamburger class="w-8 h-8 z-20 text-black"></hamburger>
-        <blob class="absolute w-36 h-36 text-yellow-500 z-10 blob"></blob>
+        <div
+          class="w-8 h-8 text-black relative z-20"
+          @click="menuOpen = !menuOpen"
+        >
+          <hamburger v-if="menuOpen === false"></hamburger>
+          <close v-else></close>
+        </div>
+        <blob
+          class="absolute text-yellow-500 z-10 blob"
+          :class="{ 'blob-open': menuOpen, 'blob-closed': !menuOpen }"
+        ></blob>
       </div>
       <div class="hidden md:block">Desktop menu stuff</div>
     </div>
+    <transition name="menu">
+      <ul
+        v-if="menuOpen"
+        class="absolute z-20 w-screen top-32 left-0 flex flex-col items-center space-y-4"
+      >
+        <router-link v-if="user" to="/profile" active-class="text-white"
+          ><h6>Profile</h6></router-link
+        >
+        <router-link v-if="user" to="/logout" active-class="text-white"
+          ><h6>Logout</h6></router-link
+        >
+        <router-link v-if="!user" to="/login" active-class="text-white"
+          ><h6>Login</h6></router-link
+        >
+        <router-link v-if="!user" to="/sign-up" active-class="text-white"
+          ><h6>Sign up</h6></router-link
+        >
+        <router-link to="/about" active-class="text-white"
+          ><h6>About</h6></router-link
+        >
+      </ul>
+    </transition>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import Hamburger from "./Hamburger.vue";
+import Close from "./Close.vue";
 import Blob from "./Blob.vue";
 export default {
   name: "Navbar",
-  components: { Hamburger, Blob },
+  components: { Hamburger, Blob, Close },
+  data() {
+    return {
+      menuOpen: false,
+    };
+  },
+  mounted() {
+    this.$router.beforeEach((to, from, next) => {
+      this.menuOpen = false;
+      next();
+    });
+  },
   computed: {
     ...mapState(["user"]),
   },
@@ -29,7 +72,26 @@ export default {
 </script>
 <style scoped>
 .blob {
-  top: -60px;
-  right: -70px;
+  transition: all 0.5s ease-in-out;
+}
+.blob-closed {
+  top: -9vh;
+  left: -6vh;
+  width: 9rem;
+  height: 9rem;
+}
+.blob-open {
+  width: 100vh;
+  height: 100vh;
+  top: -40vh;
+  left: -50vh;
+}
+.menu-enter,
+.menu-leave-to {
+  opacity: 0;
+}
+
+.menu-enter-active {
+  transition: opacity 500ms ease-in-out 500ms;
 }
 </style>
